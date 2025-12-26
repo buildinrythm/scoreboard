12c7ac28992c8201e3b325f8275fea6acafc34a4
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2025-12-26 17:27:46 +0000

    added javascript file trying to figure out scoreboard.

---
+++ index.js
@@ -0,0 +1,12 @@
+let countLeft = document.getElementById("black-box-home")
+
+countLeft = 0;
+countLeft.innerText = 5;
+console.log("testing")
+
+function increment1() {
+    
+    console.log("clicked")
+}
+
+console.log(countLeft)
