2bbab40d7dac5f52c0a8abe18621c9350ea5314c
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2025-12-27 03:51:21 +0000

    Added javascript functionality

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-let countLeft = document.getElementById("black-box-home")
+let scoreLeft = document.getElementById("black-box-home")
+let scoreRight = document.getElementById("black-box-away")
+let scoreHome = 0;
+let scoreAway = 0;
 
-countLeft = 0;
-countLeft.innerText = 5;
-console.log("testing")
 
-function increment1() {
-    
-    console.log("clicked")
+
+function incrementLeft1() {
+ scoreHome = scoreHome + 1   
+ scoreLeft.innerText = scoreHome
 }
 
-console.log(countLeft)
+function incrementLeft2() {
+ scoreHome = scoreHome + 2    
+ scoreLeft.innerText = scoreHome
+}
+
+function incrementLeft3() {
+ scoreHome = scoreHome + 3 
+ scoreLeft.innerText = scoreHome
+}
+
+function incrementRight1() {
+ scoreAway = scoreAway + 1   
+ scoreRight.innerText = scoreAway
+}
+
+function incrementRight2() {
+ scoreAway = scoreAway + 2    
+ scoreRight.innerText = scoreAway
+}
+
+function incrementRight3() {
+ scoreAway = scoreAway + 3 
+ scoreRight.innerText = scoreAway
+}
